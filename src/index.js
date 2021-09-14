e3ca4c4ca92a9f67ad483e2d7eaa4a923afb745a
PremanandChowdhury <freudigprem@gmail.com>
2021-09-15 00:57:06 +0530

    feat: Added the weather functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import './index.css'
+import Spinner from './Spinner'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: '',
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  setLat(newLat) {
+    this.setState({ lat: newLat })
+    console.log('New Lat:', this.state.lat)
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setLat(position.coords.latitude),
+      (err) => this.setState({ errorMessage: err.message })
+    )
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner message='Please Give Location Access 🙂 ' />
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
